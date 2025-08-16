9bad227e3894bcfc0d143846cc6c44bc510693b4
jagwazorld-max <jagwazorld@gmail.com>
2025-08-16 10:19:08 +0100

    Update katabump.config.js

--- katabump.config.js
+++ katabump.config.js
@@ -1,11 +1,11 @@
 module.exports = {
   entry: "bot.js",
   env: ["OWNERS"],
-  assets: ["assets/jgzbot-profile.png"],
+  assets: ["humanoid-8722271_1920.png"],
   autorestart: true,
   panel: {
     name: "J-gz-bot",
     description: "WhatsApp bot with stylish branding, secure pairing, CyperX-like commands, save view once, retrieve deleted messages, reactions, and multi-cloud deployment.",
-    image: "assets/jgzbot-profile.png"
+    image: "humanoid-8722271_1920.png"
   }
 };
